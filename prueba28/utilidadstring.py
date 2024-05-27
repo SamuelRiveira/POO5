e4d7c18c793d791d2d4698e1be7f04b748690f1a
samuelriveiraescudero <samuelriveiraescudero@gmail.com>
2024-05-27 11:50:20 +0000

    corrección definición de clase y creación jugador

--- prueba28/utilidadstring.py
+++ prueba28/utilidadstring.py
@@ -1,7 +1,7 @@
 #las constantes se ponen en mayúsculas
 DIGITOS = ["0","1","2","3","4","5","6","7","8","9"]
 
-class UtilidadString():
+class UtilidadString:
     @staticmethod
     def contarDigitos(lacadena:str) ->int:
         resultado = 0
